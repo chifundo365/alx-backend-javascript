eb74bbf1792cc0399007f8dbd3ddbd908030fdb6
= <=>
2024-08-15 07:35:00 +0000

    EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,6 +1,7 @@
-import Car from './10-car'
+/* eslint-disable class-methods-use-this */
+import Car from './10-car';
 
-export default class EVCar extends Car{
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
