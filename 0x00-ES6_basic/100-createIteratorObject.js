56732ecfc9ff5edf822b78d7bea4950b3f4d8b7a
= <=>
2024-08-15 10:24:43 +0000

    iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+/* eslint-disable guard-for-in */
+export default function createIteratorObject(report) {
+  const employees = [];
+  for (const department in report.allEmployees) {
+    employees.push(...(report.allEmployees[department]));
+  }
+
+  return employees;
+}
