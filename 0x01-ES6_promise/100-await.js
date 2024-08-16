45a250ec0ae05c313252c535b254ec39f835a38c
= <=>
2024-08-16 09:55:11 +0000

    Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,17 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const upload = await uploadPhoto();
+    const create = await createUser();
+    return {
+      photo: upload,
+      user: create,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
