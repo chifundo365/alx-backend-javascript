24a307508cf5eedd1eff61a83b50267daf666046
= <=>
2024-08-20 21:13:35 +0000

    More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unsafe-negation */
+export default function updateUniqueItems(map) {
+  if (!map instanceof Map) {
+    throw new Error('cannot process');
+  }
+  const keys = map.keys();
+  for (const k of keys) {
+    if (map.get(k) === 1) {
+      map.set(k, 100);
+    }
+  }
+  return map;
+}
