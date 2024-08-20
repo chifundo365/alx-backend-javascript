ee4fa234342b8d39852af4cd14e45455bc152468
= <=>
2024-08-20 20:07:18 +0000

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString = '') {
   const str = [];
   if (startString.length !== 0) {
     for (const item of set.values()) {
-      if ( item && item.startsWith(startString)) {
+      if (item && item.startsWith(startString)) {
         str.push(item.substring(startString.length));
       }
     }
