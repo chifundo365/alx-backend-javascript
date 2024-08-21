a6404584e9d6f4e7e6127c3820aae6e596c0b9a2
= <=>
2024-08-21 09:29:35 +0000

    Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,9 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+  const value = weakMap.get(endpoint) ? weakMap.get(endpoint) : 0;
+  weakMap.set(endpoint, value + 1);
+}
