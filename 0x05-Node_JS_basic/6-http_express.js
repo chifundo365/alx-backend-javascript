54cdd5358944f1ac2d7e5eaecef8c48b5b3ac167
= <=>
2024-10-08 19:49:57 +0000

    Create a small HTTP server using express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
+  res.setHeader('content-type', 'text/plain')
+  res.status(200);
   res.send('Hello Holberton School!');
 });
 
