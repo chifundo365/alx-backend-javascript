92c4e86e30237cb945efba778f6543126a171a3f
= <=>
2024-10-18 13:55:07 +0000

    Basic intergration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,15 +1,12 @@
 const request = require('request');
 const { expect } = require('chai')
-describe('/', () => {
-  it('Should return: 200 response statusCode', () => {
+
+describe('Index page', () => {
+  it('GET / returns correct response', (done) => {
     request('http://localhost:7865/', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
-    });
-  })
-  
-  it('Should return: Welcome to the payment sytem', () => {
-    request('http://localhost:7865', (error, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
+      done();
     });
-  })
+  });
 });
