c0802022a1a63ee4281f0b0f621a830f41975d71
= <=>
2024-10-18 13:15:56 +0000

    Basic interation testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,10 +1,15 @@
 const request = require('request');
 const { expect } = require('chai')
 describe('/', () => {
-  it('Should return: Welcome to the payment system', () => {
+  it('Should return: 200 response statusCode', () => {
     request('http://localhost:7865/', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
+    });
+  })
+  
+  it('Should return: Welcome to the payment sytem', () => {
+    request('http://localhost:7865', (error, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
     });
-  });
+  })
 });
